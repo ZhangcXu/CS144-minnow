caa7956e406e9adae43bf0faf79c0e0962e6380a
ZhangcXu <randomx200@sjtu.edu.cn>
2025-05-28 11:21:18 +0000

    check0 finished

--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -1,56 +1,80 @@
 #include "byte_stream.hh"
-
+#include "iostream"
 using namespace std;
 
-ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
+ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ){}
 
 void Writer::push( string data )
 {
-  (void)data; // Your code here.
+  if (Endflag) return;  // 不能再写了
+  std::string data_cut = "";
+  if (static_cast<uint64_t>(data.size()) > capacity_ - size_){
+    for (uint64_t idx=0; idx<capacity_ - size_; idx++){
+      data_cut += data[idx];
+    }
+    bytes_push += capacity_ - size_;
+    size_ = capacity_;
+    
+  }
+  else {
+    data_cut = data;
+    size_ += static_cast<uint64_t>(data.size());
+    bytes_push += static_cast<uint64_t>(data.size());
+  }
+  data_ += data_cut;
 }
 
 void Writer::close()
 {
-  // Your code here.
+  Endflag = true;
 }
 
 bool Writer::is_closed() const
 {
-  return {}; // Your code here.
+  return Endflag;
 }
 
-uint64_t Writer::available_capacity() const
+uint64_t Writer::available_capacity() const 
 {
-  return {}; // Your code here.
+  return capacity_ - size_;
 }
 
 uint64_t Writer::bytes_pushed() const
 {
-  return {}; // Your code here.
+  return bytes_push;
 }
 
 string_view Reader::peek() const
 {
-  return {}; // Your code here.
+  return std::string_view(data_.data(), size_);
 }
 
 void Reader::pop( uint64_t len )
 {
-  (void)len; // Your code here.
+  if (size_ < len) {
+    bytes_pop += size_;
+    data_.clear();
+    size_ = 0;
+  }
+  else{
+    bytes_pop += len;
+    data_.erase(0, static_cast<size_t>(len));
+    size_ -= len;
+  }
 }
 
 bool Reader::is_finished() const
 {
-  return {}; // Your code here.
+  return Endflag && size_ == 0;
 }
 
 uint64_t Reader::bytes_buffered() const
 {
-  return {}; // Your code here.
+  return size_;
 }
 
 uint64_t Reader::bytes_popped() const
 {
-  return {}; // Your code here.
+  return bytes_pop;
 }
 
