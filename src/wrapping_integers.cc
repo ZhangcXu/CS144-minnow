cbe871fcb3fa0d6fda7013a029acf1045e667f32
ZhangcXu <randomx200@sjtu.edu.cn>
2025-06-27 12:27:54 +0000

    64-bit & 32-bit Translation finished

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -6,13 +6,22 @@ using namespace std;
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
   // Your code here.
-  debug( "unimplemented wrap( {}, {} ) called", n, zero_point.raw_value_ );
-  return Wrap32 { 0 };
+  return Wrap32 {static_cast<uint32_t>(n + zero_point.raw_value_)};
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
   // Your code here.
-  debug( "unimplemented unwrap( {}, {} ) called", zero_point.raw_value_, checkpoint );
-  return {};
+  const uint64_t period = 1ULL << 32;   
+  auto base = static_cast<uint64_t>(raw_value_ - zero_point.raw_value_);
+  uint64_t k = checkpoint / period;
+  uint64_t cand0 = base + (k - 1) * period;
+  uint64_t cand1 = base + k * period;
+  uint64_t cand2 = base + (k + 1) * period;
+  uint64_t diff0 = cand0 > checkpoint ? cand0 - checkpoint : checkpoint - cand0;
+  uint64_t diff1 = cand1 > checkpoint ? cand1 - checkpoint : checkpoint - cand1;
+  uint64_t diff2 = cand2 > checkpoint ? cand2 - checkpoint : checkpoint - cand2;
+  if (diff0 < diff1 && diff0 < diff2) return cand0;
+  else if (diff1 < diff2) return cand1;
+  else return cand2;
 }
